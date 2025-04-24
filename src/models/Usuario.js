a03cfa87cf5f21cae2bfcf2fb7f15468209c5290
Ruan Lopes <intel.spec.lopes@gmail.com>
2025-04-24 14:12:23 -0400

    Finalizado criação schema Usuario

---
+++ src/models/Usuario.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+export class Usuario {
+    constructor() {
+        const usuarioSchema = mongoose.Schema (
+            {
+                nome: {type: String, index: true, required: true},
+                matricula: {type: String, index: true, required: true},
+                senha: {type: String, required: true},
+                cargo: {type: String, required: true},
+                data_cadastro: {type: Date, default: Date.now},
+                data_ultima_atualizacao: {type: Date, default: Date.now},
+            }
+        )
+    }
+}
