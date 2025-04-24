97ee71942585b48eece84451072726f6094cbd5f
Ruan Lopes <intel.spec.lopes@gmail.com>
2025-04-24 13:59:57 -0400

    Schema Produto Finalizado(?)

--- src/models/Produto.js
+++ src/models/Produto.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
-class Produto {
+export class Produto {
     constructor() {
         const produtoSchema = new mongoose.Schema(
             {
@@ -11,7 +11,13 @@ class Produto {
                 marca: {type: String},
                 custo: {type: Number, required: true},
                 categoria: {type: String, required: true},
-                // continuar o restante dos atributos do produto
+                estoque: {type: Number, required: true},
+                estoque_min: {type: Number, required: true},
+                data_ultima_entrada: {type: Date},
+                data_ultima_saida: {type: Date},
+                status: {type: Boolean, default: true},
+                id_fornecedor: {type: Number, required: true},
+                codigo_produto: {type: String, required: true, index: true},
             }
         )
     }
