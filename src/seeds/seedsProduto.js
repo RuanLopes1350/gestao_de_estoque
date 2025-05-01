e24f1769ea8a4d0622ab6d81ad61190140643958
Ruan Lopes <intel.spec.lopes@gmail.com>
2025-04-30 22:09:50 -0400

    AAAA

--- src/seeds/seedsProduto.js
+++ src/seeds/seedsProduto.js
@@ -1,14 +1,37 @@
 import 'dotenv/config'
-import mongoose from 'mongoose'
+import mongoose, { get } from 'mongoose'
 import DbConnect from '../config/DbConnect'
 import { Produto } from '../models/Produto'
-import { Movimentacao } from '../models/Movimentacao'
-import { Fornecedor } from '../models/Fornecedor'
-import { Usuario } from '../models/Usuario'
-import faker from 'faker-br'
+import getGlobalFakeMapping from './globalFakeMapping'
 
-const Produtos = [
-    {
-        
-    }
-]
+await DbConnect.conectar()
+
+async function seedProduto() {
+
+    await Produto.deleteMany();
+
+    const produtos_array = [];
+    const produtos = [];
+
+    produtos.push(
+        {
+            nome: getGlobalFakeMapping().produto.nome(),
+            descricao: getGlobalFakeMapping().produto.descricao(),
+            preco: getGlobalFakeMapping().produto.preco(),
+            marca: getGlobalFakeMapping().produto.marca(),
+            custo: getGlobalFakeMapping().produto.custo(),
+            categoria: getGlobalFakeMapping().produto.categoria(),
+            estoque: getGlobalFakeMapping().produto.estoque(),
+            estoque_min: getGlobalFakeMapping().produto.estoque_min(),
+            data_ultima_entrada: getGlobalFakeMapping().produto.data_ultima_entrada(),
+            data_ultima_saida: getGlobalFakeMapping().produto.data_ultima_saida(),
+            status: getGlobalFakeMapping().produto.status(),
+            id_fornecedor: getGlobalFakeMapping().produto.id_fornecedor(),
+            codigo_produto: getGlobalFakeMapping().produto.codigo_produto(),
+        }
+    )
+    
+    const resultado = await Produto.insertMany(produtos);
+    console.log("Produtos inseridos com sucesso:", resultado);
+    DbConnect.desconectar();
+}
