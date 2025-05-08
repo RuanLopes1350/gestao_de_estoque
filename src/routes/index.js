59b677183d5e0e81bb17d923f192f6bbae28900d
André Porto <andrepporto2@gmail.com>
2025-05-07 20:42:41 -0400

    app

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,28 +9,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const routes = (app) => {
-    if (process.env.DEBUGLOG) {
-        app.use(logRoutes);
-    }
-    // rota para encaminhar da raiz para /docs
-    app.get("/", (req, res) => {
-        res.redirect("/docs");
-    }
-    );
+  if (process.env.DEBUGLOG) {
+    app.use(logRoutes);
+  }
+  // rota para encaminhar da raiz para /docs
+  app.get("/", (req, res) => {
+    res.redirect("/docs");
+  });
 
-    const swaggerDocs = swaggerJsDoc(getSwaggerOptions());
-    app.use(swaggerUI.serve);
-    app.get("/docs", (req, res, next) => {
-        swaggerUI.setup(swaggerDocs)(req, res, next);
-    });
+  const swaggerDocs = swaggerJsDoc(getSwaggerOptions());
+  app.use(swaggerUI.serve);
+  app.get("/docs", (req, res, next) => {
+    swaggerUI.setup(swaggerDocs)(req, res, next);
+  });
 
-    app.use(express.json(),
-    );
+  app.use(express.json());
 
-    // Se não é nenhuma rota válida, produz 404
-    app.use((req, res) => {
-        res.status(404).json({ message: "Rota não encontrada" });
-    });
+  // Se não é nenhuma rota válida, produz 404
+  app.use((req, res) => {
+    res.status(404).json({ message: "Rota não encontrada" });
+  });
 };
 
 export default routes;
