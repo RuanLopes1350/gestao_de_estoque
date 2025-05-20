d2862dd28d7861a5ebfcf915ec60d72bff96be8b
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-05-20 18:03:38 -0400

    modificado model Usuario para permitir token para login

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -6,15 +6,20 @@ export class Usuario {
         const usuarioSchema = mongoose.Schema (
             {
                 nome_usuario: {type: String, index: true, required: true},
-                matricula: {type: String, index: true, required: true},
-                senha: {type: String, required: true},
+                matricula: {type: String, index: true, required: true, unique: true},
+                senha: {type: String, required: true, select: false},
+                email: {type: String, required: true, unique: true},
                 cargo: {type: String, required: true},
+                status: {type: Boolean, default: true},
+                acessToken: {type: String, required: true},
+                refreshToken: {type: String, required: true},
                 data_cadastro: {type: Date, default: Date.now},
-                data_ultima_atualizacao: {type: Date, default: Date.now},
+                data_atualizacao: {type: Date, default: Date.now},
+                data_ultimo_login: {type: Date, default: Date.now},
             },
             {
-                timestamps: {createdAt: 'data_cadastro', updatedAt: 'data_ultima_atualizacao'},
                 versionKey: false,
+                timestamps: {createdAt: 'data_cadastro', updatedAt: 'data_atualizacao'},
             }
         )
         usuarioSchema.plugin(mongoosePaginate);
