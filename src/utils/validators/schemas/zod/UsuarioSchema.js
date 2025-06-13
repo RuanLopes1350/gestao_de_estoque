12743ffd0a867215fecc6467a7233d161cf52a96
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-06-12 21:50:53 -0400

    recebido da master após merge da branch 64

--- src/utils/validators/schemas/zod/UsuarioSchema.js
+++ src/utils/validators/schemas/zod/UsuarioSchema.js
@@ -5,11 +5,9 @@ export const LoginSchema = z.object({
     senha: z.string().min(6, 'Senha deve ter pelo menos 6 caracteres')
 })
 
-const regexSenha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
-
 export const UsuarioSchema = z.object({
     nome_usuario: z.string().min(3).regex(/^[A-Za-zÀ-ÿ\s]+$/,'Nome do usuário deve possuir pelo menos 3 caracteres, não pode conter caracteres especiais ou numeros. Apenas letras.'),
-    matricula: z.string().min(4,'Matrícula deve conter no minimo 4 caracteres'),
+    matricula: z.string().min(7,'Matrícula deve conter no minimo 7 caracteres').max(7,'Matrícula deve conter no máximo 7 caracteres'),
     senha: z.string().min(6).regex(/[A-Z]/,'Senha deve conter pelo menos uma letra maiuscula.')
                             .regex(/[a-z]/,'Senha deve conter pelo menos uma letra minuscula.')
                             .regex(/[0-9]/,'Senha deve conter pelo menos um numero.'),
